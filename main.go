72abc06f5240981ea0816fa5e3df13fc6e36916f
Rasmus K. Kristiansen <rasmus@rasmus.kr>
2018-04-02 15:44:29 +0200

    Add usage, parameters and structure
    Require the user to specify the environment for Hel to run, and provide
    a list of service types to run.

--- main.go
+++ main.go
@@ -1,15 +1,20 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"net/http"
 	"log"
+	"net/http"
+	"os"
+	"time"
 )
 
-// Service provides different kinds of network durability impactings services.
+// Service provides different kinds of network durability impacting services.
 type Service struct {
-	serviceType string
-	replyWait int
+	endpoint string
+	port     int
+	service  string
+	delay    int
 }
 
 func (s *Service) badRequestHandler(w http.ResponseWriter, r *http.Request) {
@@ -19,21 +24,21 @@ func (s *Service) badRequestHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	case "POST":
 		fmt.Printf("Getting HTTP POST request\n")
-	    return
+		return
 	default:
-		fmt.Printf("Getting unknown type request\n")
+		fmt.Printf("Getting HTTP unknown type request\n")
+	}
+
+	if s.delay > 0 {
+		time.Sleep(time.Duration(s.delay) * 1000)
 	}
 	http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
 }
 
-func serveBadRequest() {
-	s := &Service{
-		serviceType: "badrequest",
-		replyWait: 0,
-	}
+func serveBadRequest(s Service) {
 
-	fmt.Printf("Starting %s service...\n", s.serviceType)
-	http.HandleFunc("/", s.badRequestHandler)
+	fmt.Printf("Starting %s service...\n", s.service)
+	http.HandleFunc(s.endpoint, s.badRequestHandler)
 	if err := http.ListenAndServe(":9000", nil); err != nil {
 		log.Fatal("ERROR: Cannot start server: %s\n", err)
 	}
@@ -41,6 +46,45 @@ func serveBadRequest() {
 	fmt.Printf("Stopping service...\n")
 }
 
+func displayUsage() {
+	fmt.Printf("Usage: hel OPTIONS\n")
+	fmt.Printf("  --list             Lists the provided services\n")
+	fmt.Printf("  --endpoint=PATH    Serves Hel from the PATH endpoint\n")
+	fmt.Printf("  --port=PORT        Serves Hel from the PORT TCP port number\n")
+	fmt.Printf("  --service=NAME     Use the service type NAME\n")
+	fmt.Printf("  --delay=SECONDS    Delay the response for SECONDS\n")
+	fmt.Printf("\n")
+}
+
+func displayServices() {
+	fmt.Printf("Services:\n")
+	fmt.Printf("        badrequest Returns HTTP 400 Bad Request\n")
+	fmt.Printf("\n")
+}
+
 func main() {
-	serveBadRequest()
+	listPtr := flag.Bool("list", false, "Lists the provided services")
+	endpointPtr := flag.String("endpoint", "", "Endpoint to serve")
+	portPtr := flag.Int("port", 0, "TCP port to serve from")
+	servicePtr := flag.String("service", "", "Service type, see --list")
+	delayPtr := flag.Int("delay", 0, "Delay the response")
+	flag.Parse()
+
+	if (*endpointPtr == "" || *portPtr == 0 || *servicePtr == "") && *listPtr == false {
+		displayUsage()
+		os.Exit(0)
+	}
+
+	if *listPtr == true {
+		displayServices()
+		os.Exit(0)
+	}
+
+	s := Service{
+		endpoint: *endpointPtr,
+		port:     *portPtr,
+		service:  *servicePtr,
+		delay:    *delayPtr,
+	}
+	serveBadRequest(s)
 }
