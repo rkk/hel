dc3bb5e29405373467e821375e04fad246e3c91f
Rasmus K. Kristiansen <rasmus@rasmus.kr>
2018-04-04 08:50:49 +0200

    Add more structure

--- main.go
+++ main.go
@@ -1,20 +1,34 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 	"time"
 )
 
 // Service provides different kinds of network durability impacting services.
 type Service struct {
+	input    http.Response
 	endpoint string
 	port     int
 	service  string
 	delay    int
+}
+
+// RuntimeConfiguration provides a structured configuration for application invocation.
+type RuntimeConfiguration struct {
+	listMode  bool
+	usageMode bool
+	endpoint  string
+	input     string
+	port      int
+	service   string
+	delay     int
 }
 
 func (s *Service) badRequestHandler(w http.ResponseWriter, r *http.Request) {
@@ -39,52 +53,101 @@ func serveBadRequest(s Service) {
 
 	fmt.Printf("Starting %s service...\n", s.service)
 	http.HandleFunc(s.endpoint, s.badRequestHandler)
-	if err := http.ListenAndServe(":9000", nil); err != nil {
+	if err := http.ListenAndServe(fmt.Sprintf(":%d", s.port), nil); err != nil {
 		log.Fatal("ERROR: Cannot start server: %s\n", err)
 	}
 
 	fmt.Printf("Stopping service...\n")
 }
 
+func getInput(u url.URL) (http.Response, error) {
+	var client = &http.Client{Timeout: 3 * time.Second}
+	response, err := client.Get(u.String())
+	if err != nil {
+		return http.Response{}, errors.New("Setting up HTTP client failed")
+	}
+	defer response.Body.Close()
+
+	return *response, nil
+}
+
 func displayUsage() {
 	fmt.Printf("Usage: hel OPTIONS\n")
-	fmt.Printf("  --list             Lists the provided services\n")
-	fmt.Printf("  --endpoint=PATH    Serves Hel from the PATH endpoint\n")
-	fmt.Printf("  --port=PORT        Serves Hel from the PORT TCP port number\n")
-	fmt.Printf("  --service=NAME     Use the service type NAME\n")
-	fmt.Printf("  --delay=SECONDS    Delay the response for SECONDS\n")
+	fmt.Printf("  --list             - Lists the provided services\n")
+	fmt.Printf("  --input=URL        - Retrieve input data from URL\n")
+	fmt.Printf("  --endpoint=PATH    - Serves Hel from the PATH endpoint\n")
+	fmt.Printf("  --port=PORT        - Serves Hel from the PORT TCP port number\n")
+	fmt.Printf("  --service=NAME     - Use the service type NAME\n")
+	fmt.Printf("  --delay=SECONDS    - Delay the response for SECONDS\n")
 	fmt.Printf("\n")
 }
 
 func displayServices() {
 	fmt.Printf("Services:\n")
-	fmt.Printf("        badrequest Returns HTTP 400 Bad Request\n")
+	fmt.Printf("  badrequest  - Returns HTTP 400 Bad Request\n")
 	fmt.Printf("\n")
 }
 
-func main() {
+func buildRuntimeConfiguration() (RuntimeConfiguration, error) {
 	listPtr := flag.Bool("list", false, "Lists the provided services")
 	endpointPtr := flag.String("endpoint", "", "Endpoint to serve")
+	inputPtr := flag.String("input", "", "Retrieve data from this URL")
 	portPtr := flag.Int("port", 0, "TCP port to serve from")
 	servicePtr := flag.String("service", "", "Service type, see --list")
 	delayPtr := flag.Int("delay", 0, "Delay the response")
 	flag.Parse()
 
-	if (*endpointPtr == "" || *portPtr == 0 || *servicePtr == "") && *listPtr == false {
-		displayUsage()
-		os.Exit(0)
+	var usageMode = false
+	if (*endpointPtr == "" || *inputPtr == "" || *portPtr == 0 || *servicePtr == "") && *listPtr == false {
+		usageMode = true
 	}
 
-	if *listPtr == true {
+	runtime := RuntimeConfiguration{
+		listMode:  *listPtr,
+		usageMode: usageMode,
+		endpoint:  *endpointPtr,
+		input:     *inputPtr,
+		port:      *portPtr,
+		service:   *servicePtr,
+		delay:     *delayPtr,
+	}
+	return runtime, nil
+}
+
+func main() {
+
+	config, err := buildRuntimeConfiguration()
+	if err != nil {
+		log.Fatal("ERROR: Invalid runtime configuration")
+		os.Exit(1)
+	}
+
+	if config.listMode == true {
 		displayServices()
 		os.Exit(0)
 	}
 
+	if config.usageMode == true {
+		displayUsage()
+		os.Exit(0)
+	}
+	u, err := url.Parse(config.input)
+	if err != nil {
+		log.Fatal("ERROR: %s is not a valid input URL\n", config.input)
+		os.Exit(1)
+	}
+
+	input, err := getInput(*u)
+	if err != nil {
+		log.Fatal("ERROR: Cannot retrieve contents of %s", u.String())
+	}
+
 	s := Service{
-		endpoint: *endpointPtr,
-		port:     *portPtr,
-		service:  *servicePtr,
-		delay:    *delayPtr,
+		endpoint: config.endpoint,
+		input:    input,
+		port:     config.port,
+		service:  config.service,
+		delay:    config.delay,
 	}
 	serveBadRequest(s)
 }
